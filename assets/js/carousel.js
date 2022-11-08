1c990ac8b2a95c50ef30f51dee4cf91aba0f988c
veroni1337 <aliserb773@gmail.com>
2022-11-08 20:01:55 +0200

    fixed

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -6,3 +6,12 @@ var swiper = new Swiper(".calendar_swiper", {
         prevEl: ".calendar_swiper_prev",
     },
 });
+
+var swiper = new Swiper(".ask_question_swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    allowTouchMove: false,
+    navigation: {
+        nextEl: ".ask_question_next button",
+    },
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,68 @@ storieVideoPlay.forEach(trigger => {
     })
 })
 // stories video play
+
+var toggle = document.getElementById('container');
+var toggleContainer = document.getElementById('toggle-container');
+var toggleNumber;
+
+toggle.addEventListener('click', function() {
+	toggleNumber = !toggleNumber;
+	if (toggleNumber) {
+		toggleContainer.style.clipPath = 'inset(0 0 0 50%)';
+		toggleContainer.style.backgroundColor = '#E7C99F';
+        toggleContainer.style.border = '0px';
+	} else {
+		toggleContainer.style.clipPath = 'inset(0 50% 0 0)';
+		toggleContainer.style.backgroundColor = '#E7C99F';
+        toggleContainer.style.border = '0px';
+	}
+	console.log(toggleNumber)
+});
+
+var toggleTriple = document.getElementById('containerTriple');
+var toggleContainerTriple = document.getElementById('toggle-container-triple');
+var toggleNumberTriple;
+
+document.getElementById('triple_toggle-1').addEventListener('click', () => {
+    toggleContainerTriple.style.clipPath = 'inset(0px 66% 0px 0px)';
+    toggleContainerTriple.style.backgroundColor = '#E7C99F';
+    toggleContainerTriple.style.border = '0px';
+})
+
+document.getElementById('triple_toggle-2').addEventListener('click', () => {
+    toggleContainerTriple.style.clipPath = 'inset(0px 33% 0px 33%)';
+    toggleContainerTriple.style.backgroundColor = '#E7C99F';
+    toggleContainerTriple.style.border = '0px';
+})
+
+document.getElementById('triple_toggle-3').addEventListener('click', () => {
+    toggleContainerTriple.style.clipPath = 'inset(0px 0px 0px 66%)';
+    toggleContainerTriple.style.backgroundColor = '#E7C99F';
+    toggleContainerTriple.style.border = '0px';
+})
+
+// ask question modal
+let askModalTrigger = document.querySelectorAll('.ask_question_trigger');
+let askModal = document.querySelector('.ask_question');
+
+askModalTrigger.forEach(trigger => {
+    trigger.addEventListener('click', event => {
+        if (askModal.classList.contains('active')) {
+            askModal.classList.remove('active');
+        } else {
+            askModal.classList.add('active');
+            document.body.classList.add('overflow_hidden');
+        }
+    })
+})
+
+let askModalClose = document.querySelectorAll('.ask_modal_close');
+
+askModalClose.forEach(trigger => {
+    trigger.addEventListener('click', close => {
+        askModal.classList.remove('active');
+        document.body.classList.remove('overflow_hidden');
+    })
+})
+// ask question modal
